3f2b69fbeb9507f32b188aebeb8855baaeaf863c
Error 404 <90735784+Errno404NotFound@users.noreply.github.com>
2021-10-13 14:17:49 -0400

    Update SoD.sol

--- ERC721/SoD/SoD.sol
+++ ERC721/SoD/SoD.sol
@@ -2,32 +2,58 @@
 
 pragma solidity ^0.8.0;
 
-import "./ExtraContracts/ERC721.sol";
-import "./ExtraContracts/Ownable.sol";
-import "./ExtraContracts/SafeMath.sol";
+import "../ExtraContracts/ERC721.sol";
+import "../ExtraContracts/Ownable.sol";
+import "../ExtraContracts/ReentrancyGuard.sol";
+import "../ExtraContracts/MerkleProof.sol";
 
-contract PIXLS is ERC721, Ownable {
-    using Strings for uint256;
-    using SafeMath for uint256;
-    
-    uint256 public maxSwords = 2000; // Max supply
-    uint256 public teamReserved = 15; // 
-    uint256 public price = .025 ether;
-    uint256 public maxPerTx = 10;
-    uint256 public totalSupply = 0;
+contract SoDWhitelist is ERC721, Ownable, ReentrancyGuard {
+
+    using MerkleProof for *;
+
+    bytes32 public immutable merkleRoot; // Used for whitelist check
+
+    uint256 public maxSwords = 2222; // Max supply
+    uint256 public teamReserved = 50; // 15 for team, 35 for events/giveaways
+    uint256 public price = .025 ether; 
+    uint256 public maxPerTx = 10; 
+    uint256 public totalSupply = 0; 
     uint256 public mintingStartTime = 3093527998799; // Change starting time, currently set to year 99999
+    uint256 public presaleStartTime = 3093527998799; // Change starting time, currently set to year 99999
     
-    bool public licenseLocked = false;
+    bool public licenseLocked = false; // Once locked nothing can be changed anymore
     
-    string private baseURI;
+    string private baseURI; // Site api link for OS to pull metadata from
 
-    constructor() ERC721("Swords Of Destiny", "SWORD") {
-        // Add minting 1 at launch
+    mapping(address => uint256) public mintedPerAccount; // Minted per whitelisted account
+
+    constructor(string _name, string _tokenName) ERC721(_name, _tokenName) Ownable() ReentrancyGuard() {
+
     }
+
+    // Claim whitelisted tokens using merkle tree
+    function claim(uint256 index, address account, uint256 amountReserved, uint256 amountToMint, bytes32[] calldata merkleProof) external {
+        require(merkleRoot, "Root has not yet been set");
+        require(block.timestamp >= presaleStartTime, "Presale has not started yet");
+        require(amountToMint + mintedPerAccount[account] < amountReserved, "Cannot mint more than reserved");
+
+        // Verify the merkle proof to make sure given information matches whitelist saved info.
+        bytes32 node = keccak256(abi.encodePacked(index, account, amountReserved));
+        require(MerkleProof.verify(merkleProof, merkleRoot, node), "Invalid proof.");
+
+        mintedPerAccount[account] += amountToMint;
         
-    function mint(uint256 _amount) public payable {
+        for (uint256 i = 0; i < amountToMint; i++) {
+            _safeMint(account, totalSupply + i);
+        }
+
+        totalSupply += amountToMint;
+    }
+
+    // 
+    function mint(uint256 _amount) nonReentrant() public payable {
         require(totalSupply < maxSwords, "Sale has already ended");
-        require(block.timestamp >= mintingStartTime, "It's not time to mint yet");
+        require(block.timestamp >= mintingStartTime, "Sale has not started yet");
         require(_amount <= maxPerTx, "Cannot mint more than 10 tokens per transaction");
         require(totalSupply + _amount <= maxSwords - teamReserved, "Cannot exceed max supply");
         require(msg.value >= price * _amount, "Ether sent is not correct");
@@ -37,6 +63,23 @@ contract PIXLS is ERC721, Ownable {
         }
         
         totalSupply += _amount;
+    }
+
+    function teamMint(address _to, uint256 _amount) external onlyOwner {
+        require(_amount > 0, "Can't mint 0 tokens");
+        require(_amount <= teamReserved, "Cannot exceed reserved supply");
+        
+        for(uint256 i; i < _amount; i++){
+            _safeMint(_to, totalSupply + i);
+        }
+        
+        teamReserved -= _amount;
+        totalSupply += _amount;
+    }
+
+    function setMerkleRoot(bytes32 _root) public onlyOwner {
+        require(!merkleRoot, "Root already set, no changes can be made to it.");
+        merkleRoot = _root;
     }
 
     function setMintingStartTime(uint256 _startTime) public onlyOwner {
@@ -56,18 +99,6 @@ contract PIXLS is ERC721, Ownable {
     
     function _baseURI() internal view virtual override returns (string memory) {
         return baseURI;
-    }
-    
-    function teamMint(address _to, uint256 _amount) external onlyOwner {
-        require(_amount > 0, "Can't mint 0 tokens");
-        require(_amount <= teamReserved, "Cannot exceed reserved supply");
-        
-        for(uint256 i; i < _amount; i++){
-            _safeMint(_to, totalSupply + i);
-        }
-        
-        teamReserved -= _amount;
-        totalSupply += _amount;
     }
     
     function withdraw(address _to, uint256 _amount) public onlyOwner {
